2e3e6a435857a73967d209281b6366806e2966f2
Patcharapong10 <patcharapong20new@gmail.com>
2021-02-22 22:11:37 +0700

    commit

---
+++ PyRedis.py
@@ -0,0 +1,73 @@
+import os
+import json
+import redis
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+db = redis.StrictRedis(
+        host='10.100.2.131',
+        port=6379,
+        password='ICLmps27364'
+        ,decode_responses=True)
+
+
+@app.route('/',methods=['GET'])
+def show():
+    name=db.keys()
+    name.sort()
+    req = []
+    for z in name :
+        req.append(db.hgetall(z))
+    return jsonify(req)
+    #return 'Hello %s!' % name
+
+
+
+# Get Single Key
+@app.route('/<Key>',methods=['GET'])
+def get_key(Key):
+    result = db.hgetall(Key)
+    return jsonify(result)
+
+# DELETE Key
+@app.route('/<Key>',methods=['DELETE'])
+def DELETE_key(Key):
+    result = db.delete(Key)
+    return jsonify(result)
+
+ # Post Key
+@app.route('/',methods=['POST'])
+def Post_key():
+    brand = request.json['brand']
+    color = request.json['color']
+    size = request.json['size']
+    windows = request.json['windows']
+    data = {"brand":brand, "color":color, "size":size , "windows":windows}
+
+    db.hmset(windows,data)
+    
+    return jsonify(data)
+
+# update Key
+@app.route('/<Key>',methods=['PUT'])
+def PUT_key(Key):
+
+    brand = request.json['brand']
+    color = request.json['color']
+    size = request.json['size']
+    windows = request.json['windows']
+    data = {"Key":Key, "brand":brand,  "color":color, "size":size}
+
+    db.hmset(Key,data)
+    return jsonify(data)
+
+
+
+
+#@app.route('/setname/<name>')
+#def setname(name):
+#    db.set('name',name)
+#   return 'Name updated.'
+
+if __name__ == '__main__':
+    app.run()
